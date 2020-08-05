d7f318f37e91e8aebf9a75b012419927fa0d3eae
Julian Narvaez <oscarnarvaez01@hotmail.com>
2020-08-04 22:49:11 -0500

    unable to get data since MySQL (example db  sakila - table actor)

--- Backend/app/models/mysql_model.js
+++ Backend/app/models/mysql_model.js
@@ -1,5 +1,23 @@
-const mysql_model = function(){
-    
+const mysql_model = function(MySQL){
+
+    this.getAll=function(){
+        return new Promise((resolve,reject)=>{
+            MySQL.connect(function(err) {
+                if (err){
+                    reject(err);
+                }else{
+                    MySQL.query("SELECT * FROM actor", function (err, result, fields) {
+                        if (err) throw err;
+                        console.log(fields);
+                        resolve(result);
+                    });
+                    
+                }
+                
+            });
+        });
+    };
+    return this;
 };
 
 module.exports = mysql_model;
--- Backend/index.js
+++ Backend/index.js
@@ -2,30 +2,17 @@ const express = require("express");
 const app = express();
 const config = require("./config.json");
 const port = process.env.PORT?process.env.PORT:config.app.port?config.app.port:3456;//SELECT ANY PORT
-const Sequelize = require("sequelize");
 const cors = require("cors");
 
-//MYSQL SERVER CONNECTION
-const sequelize = new Sequelize('e_commerce', 'root', 'root', {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql'
-  });
-
-  async function sql_con(){
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-  };
-
+let cardController = require('./app/utils/controller')('card');
 //SERVER CONNECTION
 app.use(cors());
+
+//CONTROLLERS
+app.use('/card',cardController);
+
 app.listen(port, () => {
     console.log("Running server on port "+port);
-    sql_con();
 });
 
 
