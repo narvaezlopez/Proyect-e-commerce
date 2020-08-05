3102cc6336256eb30fc9b008dc570465c938cca9
unknown <josebroldant@gmail.com>
2020-08-05 12:01:39 -0500

    added conn ctrlr, query to insert data, added bodyparser

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,13 @@ const app = express();
 const config = require("./config.json");
 const port = process.env.PORT?process.env.PORT:config.app.port?config.app.port:3456;//SELECT ANY PORT
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
-let cardController = require('./app/utils/controller')('card');
+let cardController = require('./app/utils/controller')('actor');
 //SERVER CONNECTION
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 //CONTROLLERS
 app.use('/card',cardController);
