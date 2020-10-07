803754b7f633c2fba49dbd0e5797305ebb0d3d76
oscarnarvaez01@hotmail.com <juliannarvaez@BOMC9081969.local>
2020-10-07 12:24:45 -0500

    firmado

--- Backend/index.js
+++ Backend/index.js
@@ -19,14 +19,12 @@ let rolController = require('./app/utils/controller')('rol');
 let user_rolController = require('./app/utils/controller')('user_rol');
 let categoriesController = require('./app/utils/controller')('categories');
 
-//SERVER CONNECTION
-//commit firma
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 //CONTROLLERS
-app.use('/e_user', e_userController);
+
 app.use('/state', stateController);
 app.use('/picture_product', picture_productController);
 app.use('/inventory_product', inventory_productController);
